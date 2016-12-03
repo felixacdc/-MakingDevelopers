108e63b96843da7695f02c14eadc83dd1dda5860
felixacdc <pfmata01@gmail.com>
2016-12-02 20:17:35 -0600

    [add] Desarrollando un CMS desde cero con Node.js Express, Handlebars, Stylus, Gulp y más: Config #5

---
+++ src/lib/env.js
@@ -0,0 +1,13 @@
+module.exports = getEnv;
+
+/** 
+ * 
+ * Gets the current enviroment based on NODE_ENV var.
+ * 
+ */
+
+function getEnv() {
+    return {
+        name: process.env.NODE_ENV ? process.env.NODE_ENV : 'production'
+    }
+}
