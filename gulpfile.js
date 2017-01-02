e51278dd30e54abfe4ea8ae53447f3d02aa885f3
felixacdc <pfmata01@gmail.com>
2017-01-01 22:19:19 -0600

    [modify] Desarrollando un CMS desde cero con Node.js Express, Handlebars, Stylus, Gulp y más: Babel #6

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,32 @@
-var gulp = require('gulp');
-var mocha = require('gulp-mocha');
+import eslint from 'gulp-eslint';
+import gulp from 'gulp';
+import livereload from 'gulp-livereload';
+import nodemon from 'gulp-nodemon';
+import notify from 'gulp-notify';
+import stylus from 'gulp-stylus';
+import mocha from 'gulp-mocha';
 
-// Imported tasks
-gulp.task('lint', require('./gulp/eslint'));
+// Linter task
+gulp.task('eslint', function () {
+    return gulp.src([
+        'src/**/*.js',
+        '!src/public/bower_components/**/*.js'
+    ])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
 
-// tasks
-gulp.task('analyze', ['lint']);
-gulp.task('pre-push', ['lint']);
+// Stylus task
+gulp.task('stylus', function () {
+    return gulp.src('src/stylus/style.styl')
+    .pipe(stylus({
+        compress: true
+    }))
+    .pipe(livereload())
+    .pipe(gulp.dest('src/public/css'));
+});
 
-// Mocha task
-gulp.task('test', () => {
-  return gulp.src([
-    'test/**/*Test.js'
-  ])
-  .pipe(mocha());
+gulp.task('livereload', () => {
+    livereload({ start: true });
 });
